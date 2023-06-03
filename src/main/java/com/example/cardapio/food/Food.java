f162b2c420b6a054658e427889af58720e35ac3d
IgorMontezuma20 <igorminranda17@gmail.com>
2023-06-03 15:16:42 -0300

    first commit

---
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -0,0 +1,30 @@
+package com.example.cardapio.food;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Food {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "image", nullable = true)
+    private String image;
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    public Food(FoodRequestDTO data){
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
+    }
+}
